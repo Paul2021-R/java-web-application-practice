5e70931e293473477f49cc5dc39af8b89df35044
haryu <haryu@student.42seoul.kr>
2023-10-25 10:19:02 +0900

    FEAT: set ready to study chapter 84

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SyHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SyHelloController.java
@@ -1,4 +1,29 @@
 package com.in28minutes.springboot.myfirstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SyHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello What are learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHTML() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<HTML>" +
+                "<head>" +
+                "<title>" +
+                "My first HTML Page</title>" +
+                "<h1>My first html page with body</h1>" +
+                "</body>" +
+                "</html>");
+        return sb.toString();
+    }
 }
